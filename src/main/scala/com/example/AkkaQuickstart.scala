5dfc36ee01ff024cc1c6d751909647c5bd720168
Eric Semeniuc <eric.semeniuc@salesforce.com>
2019-06-13 15:22:38 -0700

    finished

--- src/main/scala/com/example/AkkaQuickstart.scala
+++ src/main/scala/com/example/AkkaQuickstart.scala
@@ -1,3 +1,4 @@
+package com.example
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
@@ -5,11 +6,9 @@ import akka.http.scaladsl.model.HttpMethods._
 import akka.http.scaladsl.model._
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Sink
-import com.example.{DecodeURL, PageContent}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.unmarshalling.Unmarshal
-import akka.http.scaladsl.unmarshalling.Unmarshaller
-
+import akka.http.scaladsl.model.headers
 import scala.collection.mutable
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
@@ -17,48 +16,64 @@ import scala.io.StdIn
 import scala.util.Random
 
 object AkkaQuickstart {
+  val rootUrl = "http://localhost:8080"
+  private implicit val system = ActorSystem()
+  private implicit val materializer = ActorMaterializer()
+  private implicit val executionContext = system.dispatcher
+  private val urlMap = mutable.Map[String, String]("" -> rootUrl)
+
+  private def requestHandler(req: HttpRequest): HttpResponse = {
+
+    req match {
+      case HttpRequest(GET, Uri.Path("/"), _, _, _) =>
+        HttpResponse(entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          PageContent.header() + PageContent.homepageContent() + PageContent.footer()))
+
+      case HttpRequest(POST, Uri.Path("/submit"), _, entity, _) =>
+        val url = Await.result(Unmarshal(req).to[String], 1.second)
+        val (decodedUrl, ok) = DecodeURL(url)
+        println(s"user submitted: $decodedUrl")
+        if (ok) {
+          val randomString = Random.alphanumeric.take(10).mkString
+          urlMap(randomString) = decodedUrl
+          val shortenedUrl = s"$rootUrl/$randomString"
+          return HttpResponse(entity = HttpEntity(ContentTypes.`text/html(UTF-8)`,
+            PageContent.header + s"Success, go to <a href=$shortenedUrl>$shortenedUrl</a>" + PageContent.footer()))
+        }
+        HttpResponse(entity = "Error in post request!")
+
+      case default => //catch all
+        val shortcodeParts = req.getUri().path().split("/")
+        default.discardEntityBytes() // important to drain incoming HTTP Entity stream
+        if (shortcodeParts.length != 2) {
+          return HttpResponse(entity = "Error in shortcode!")
+        }
+        val shortcode = shortcodeParts(1)
+        println(s"shortcode: $shortcode")
+        if (!urlMap.contains(shortcode)) {
+          val shortenedUrl = s"$rootUrl/$shortcode"
+          return HttpResponse(404, entity = HttpEntity(ContentTypes.`text/html(UTF-8)`,
+            PageContent.header + s"Sorry, couldn't find $shortenedUrl" + PageContent.footer()))
+        }
+
+        HttpResponse(
+          status = StatusCodes.TemporaryRedirect,
+          headers = List(headers.Location(urlMap(shortcode))),
+          entity = ""
+        )
+    }
+  }
+
   def main(args: Array[String]) {
-    implicit val system = ActorSystem()
-    implicit val materializer = ActorMaterializer()
-    implicit val executionContext = system.dispatcher
 
     val serverSource = Http().bind(interface = "localhost", port = 8080)
 
-    var urlMap = mutable.Map[String, String]()
-    val requestHandler = (req: HttpRequest) => {
-      println(s"req : $req")
-      req match {
-        case HttpRequest(GET, Uri.Path("/"), _, _, _) =>
-          HttpResponse(entity = HttpEntity(
-            ContentTypes.`text/html(UTF-8)`,
-            PageContent.header() + PageContent.homepageContent() + PageContent.footer()))
-
-        case HttpRequest(POST, Uri.Path("/submit"), _, entity, _) =>
-          val url = Await.result(Unmarshal(req).to[String], 1.second)
-          val (decodedUrl, ok) = DecodeURL(url)
-          println(s"output: $decodedUrl")
-          if (ok) {
-            val randomString = Random.alphanumeric.take(10).mkString
-            urlMap(decodedUrl) = randomString
-            val shortenedUrl = s"http://localhost:8080/$randomString"
-            HttpResponse(entity = HttpEntity(ContentTypes.`text/html(UTF-8)`,
-              PageContent.header + s"Success, go to <a href=$shortenedUrl>$shortenedUrl</a>" + PageContent.footer()))
-          } else {
-            HttpResponse(entity = "Error in post request!")
-          }
-
-
-        case default =>
-          println(s"uri ${default.getUri()}")
-          default.discardEntityBytes() // important to drain incoming HTTP Entity stream
-          HttpResponse(404, entity = "Unknown resource!")
-      }
-    }
-
     val bindingFuture: Future[Http.ServerBinding] =
-      serverSource.to(Sink.foreach { connection =>
-        println("Accepted new connection from " + connection.remoteAddress)
-        connection.handleWithSyncHandler(requestHandler)
+      serverSource.to(Sink.foreach {
+        connection =>
+          println("Accepted new connection from " + connection.remoteAddress)
+          connection.handleWithSyncHandler(requestHandler)
         // this is equivalent to
         // connection handleWith { Flow[HttpRequest] map requestHandler }
       }).run()
@@ -68,4 +83,5 @@ object AkkaQuickstart {
     bindingFuture.flatMap(_.unbind()) // trigger unbinding from the port
       .onComplete(_ => system.terminate()) // and shutdown when done
   }
+
 }
--- src/main/scala/com/example/DecodeURL.scala
+++ src/main/scala/com/example/DecodeURL.scala
@@ -1,14 +1,21 @@
 package com.example
 
 import java.net.URLDecoder
+import AkkaQuickstart.rootUrl
 
 object DecodeURL {
   def apply(encodedUrl: String): (String, Boolean) = {
-    val splitUrl = encodedUrl.split("urlField=")
-    if (splitUrl.length < 2) {
+    val parts = encodedUrl.split("urlField=")
+    if (parts.length < 2) {
       return ("", false)
     }
-    val percentEncoding = splitUrl(1).replace("+", "%20")
+    val percentEncoding = parts(1).replace("+", "%20")
     (URLDecoder.decode(percentEncoding, "UTF-8"), true)
+  }
+
+  def getUrlShortcode(url: String): String = {
+    val parts = url.split(rootUrl)
+
+    return ""
   }
 }
